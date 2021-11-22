15c0d30830e910b189124737731a459ba1cc2de4
Ana Ulin <ana@ulin.org>
2021-11-22 13:30:15 -0800

    Add logging configuration to server.

--- starling-capture-api/main.py
+++ starling-capture-api/main.py
@@ -1,8 +1,29 @@
+from re import S
 from aiohttp import web
 
 import handlers
+import logging
+import sys
 
-if __name__ == '__main__':
+_logger = logging.getLogger(__name__)
+
+
+def configure_logging():
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S%z"
+    )
+
+
+def start_server():
     app = web.Application()
-    app.add_routes([web.get('/image', handlers.process_image)])
+    app.add_routes([web.get("/image", handlers.process_image)])
+    _logger.info("Starting up server")
     web.run_app(app)
+
+
+if __name__ == "__main__":
+    configure_logging()
+    start_server()
