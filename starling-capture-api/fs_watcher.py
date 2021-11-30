a204e2564fce0f5b5bd4bb87aba22e294f9eb744
Benedict Lau <b@deprecated.systems>
2021-11-29 21:03:07 -0500

    Add fs watch and multiprocessing

---
+++ starling-capture-api/fs_watcher.py
@@ -0,0 +1,41 @@
+import time  
+from watchdog.observers import Observer  
+from watchdog.events import PatternMatchingEventHandler
+
+class FsWatcher:
+    """Watches directories for file changes."""
+
+    def watch(self, dir_path):
+        observer = Observer()
+        observer.schedule(self.Handler(), recursive=True, path=dir_path)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
+    class Handler(PatternMatchingEventHandler):
+        """Handles file changes."""
+        patterns = ["*.jpg", "*.jpeg"]
+
+        def process(self, event):
+            """Processes file change event.
+
+            Args:
+                event: the file change event
+            """
+            print(event.src_path, event.event_type, event.is_directory)
+
+        def on_created(self, event):
+            self.process(event)
+
+        def on_modified(self, event):
+            self.process(event)
+
+        def on_moved(self, event):
+            self.process(event)
+
+        def on_deleted(self, event):
+            self.process(event)
--- starling-capture-api/main.py
+++ starling-capture-api/main.py
@@ -1,12 +1,46 @@
 from aiohttp import web
 from aiohttp_jwt import JWTMiddleware
+from fs_watcher import FsWatcher
 
 import config
 import handlers
 import logging
+import multiprocessing
+import os
+import signal
 import sys
+import time
 
 _logger = logging.getLogger(__name__)
+_procs = list()
+
+def signalHandler(signum, frame):
+    '''
+    SIGINT handler for the main process.
+    '''
+    print('Terminating processes...')
+    killProcesses(_procs)
+    sys.exit(0)
+
+
+def killProcesses(procs):
+    for proc in procs:
+        if proc.is_alive():
+            try:
+                proc.terminate()
+
+                # Allow up to 10 seconds for the process to terminate
+                i = 0
+                while proc.is_alive() and i < 20:
+                    time.sleep(0.5)
+                    i += 1
+            except os.error:
+                pass
+
+        if proc.is_alive():
+            print('Process %s [%s] is not terminated' % (proc.pid, proc.name))
+        else:
+            print('Process %s [%s] is terminated' % (proc.pid, proc.name))
 
 
 def configure_logging():
@@ -18,7 +52,8 @@ def configure_logging():
     )
 
 
-def start_server():
+def start_api_server():
+    configure_logging()
     app = web.Application(
         middlewares=[
             JWTMiddleware(
@@ -27,10 +62,24 @@ def start_server():
         ]
     )
     app.add_routes([web.post("/assets/create", handlers.create)])
-    _logger.info("Starting up server")
+    _logger.info("Starting up API server")
     web.run_app(app)
 
 
-if __name__ == "__main__":
+def start_fs_watcher():
     configure_logging()
-    start_server()
+    _logger.info("Starting up file system watcher")
+    FsWatcher().watch(config.IMAGES_DIR)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signalHandler)
+    configure_logging()
+
+    proc_fs_watcher = multiprocessing.Process(name='proc_fs_watcher', target=start_fs_watcher)
+    _procs.append(proc_fs_watcher)
+    proc_api_server = multiprocessing.Process(name='proc_api_server', target=start_api_server)
+    _procs.append(proc_api_server)
+    
+    proc_fs_watcher.start()
+    proc_api_server.start()
