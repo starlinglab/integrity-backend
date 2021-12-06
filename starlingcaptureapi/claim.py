2442b106ec5a37695a8856c191e67a5b16d04491
Ana Ulin <ana@ulin.org>
2021-12-06 15:50:06 -0800

    Refactor Claim generation to only load templates at server startup. Add basic tests for all actions.

--- starlingcaptureapi/claim.py
+++ starlingcaptureapi/claim.py
@@ -1,5 +1,33 @@
+import copy
 import json
+import logging
 import os
+
+_logger = logging.getLogger(__name__)
+
+
+def _load_template(filename):
+    """Loads a claim template JSON file.
+
+    Args:
+        filename: basename of file to load, relative to the claims template directory
+
+    Return:
+        a dictionary with the loaded claim template
+    """
+    full_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"c2pa_claims/{filename}"
+    )
+    with open(full_path, "r") as claim_file:
+        template = json.load(claim_file)
+        _logger.info("Successfully loaded claim template: %s", filename)
+        return template
+
+
+# At code load time, read our claim JSON template files.
+CREATE_CLAIM_TEMPLATE = _load_template("claim_create.json")
+UPDATE_CLAIM_TEMPLATE = _load_template("claim_update.json")
+STORE_CLAIM_TEMPLATE = _load_template("claim_store.json")
 
 
 class Claim:
@@ -14,33 +42,27 @@ class Claim:
         Returns:
             a dictionary containing the 'create' claim data
         """
-        claim_file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "c2pa_claims/claim_create.json"
-        )
-        with open(claim_file_path, "r") as claim_file:
-            claim = json.load(claim_file)
+        claim = copy.deepcopy(CREATE_CLAIM_TEMPLATE)
 
-            # Replace claim values with Starling Lab defaults.
-            claim["vendor"] = "starlinglab"
-            claim["recorder"] = "Starling Capture"
+        # Replace claim values with Starling Lab defaults.
+        claim["vendor"] = "starlinglab"
+        claim["recorder"] = "Starling Capture"
 
-            # Replace claim values with values from JWT payload.
-            for assertion in claim["assertions"]:
-                if assertion["label"] == "stds.schema-org.CreativeWork":
-                    assertion["data"]["author"][0]["identifier"] = jwt_payload[
-                        "author"
-                    ]["identifier"]
-                    assertion["data"]["author"][0]["name"] = jwt_payload["author"][
-                        "name"
-                    ]
-                    continue
-                if assertion["label"] == "stds.iptc.photo-metadata":
-                    assertion["data"]["dc:creator"] = [jwt_payload["author"]["name"]]
-                    assertion["data"]["dc:rights"] = jwt_payload["copyright"]
-                    continue
+        # Replace claim values with values from JWT payload.
+        for assertion in claim["assertions"]:
+            if assertion["label"] == "stds.schema-org.CreativeWork":
+                assertion["data"]["author"][0]["identifier"] = jwt_payload["author"][
+                    "identifier"
+                ]
+                assertion["data"]["author"][0]["name"] = jwt_payload["author"]["name"]
+                continue
+            if assertion["label"] == "stds.iptc.photo-metadata":
+                assertion["data"]["dc:creator"] = [jwt_payload["author"]["name"]]
+                assertion["data"]["dc:rights"] = jwt_payload["copyright"]
+                continue
 
-            # Replace claim values with values from HTTP POST.
-            # TODO
+        # Replace claim values with values from HTTP POST.
+        # TODO
 
         return claim
 
@@ -50,11 +72,7 @@ class Claim:
         Returns:
             a dictionary containing the 'update' claim data
         """
-        claim_file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "c2pa_claims/claim_update.json"
-        )
-        with open(claim_file_path, "r") as claim_file:
-            claim = json.load(claim_file)
+        claim = copy.deepcopy(UPDATE_CLAIM_TEMPLATE)
         return claim
 
     def generate_store(self, ipfs_cid):
@@ -66,20 +84,15 @@ class Claim:
         Returns:
             a dictionary containing the 'store' claim data
         """
-        claim_file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "c2pa_claims/claim_store.json"
-        )
-        with open(claim_file_path, "r") as claim_file:
-            claim = json.load(claim_file)
-
-            # Replace claim values.
-            for assertion in claim["assertions"]:
-                if assertion["label"] == "org.starlinglab.storage.ipfs":
-                    assertion["data"]["starling:Provider"] = "Web3.Storage"
-                    assertion["data"]["starling:IpfsCid"] = ipfs_cid
-                    # TODO
-                    assertion["data"]["starling:AssetStoredTimestamp"] = ""
-                    break
+        claim = copy.deepcopy(STORE_CLAIM_TEMPLATE)
+        # Replace claim values.
+        for assertion in claim["assertions"]:
+            if assertion["label"] == "org.starlinglab.storage.ipfs":
+                assertion["data"]["starling:Provider"] = "Web3.Storage"
+                assertion["data"]["starling:IpfsCid"] = ipfs_cid
+                # TODO
+                assertion["data"]["starling:AssetStoredTimestamp"] = ""
+                continue
 
         return claim
 
--- tests/test_claim.py
+++ tests/test_claim.py
@@ -15,6 +15,26 @@ def test_generates_create_claim():
     assertions = _claim.assertions_by_label(claim)
     assert claim["vendor"] == "starlinglab"
     assert claim["recorder"] == "Starling Capture"
-    assert assertions["stds.schema-org.CreativeWork"]["data"]["author"][0]["name"] == "Jane Doe"
-    assert assertions["stds.iptc.photo-metadata"]["data"]["dc:creator"] == [ "Jane Doe" ]
-    assert assertions["stds.iptc.photo-metadata"]["data"]["dc:rights"] == "copyright holder"
+    assert (
+        assertions["stds.schema-org.CreativeWork"]["data"]["author"][0]["name"]
+        == "Jane Doe"
+    )
+    assert assertions["stds.iptc.photo-metadata"]["data"]["dc:creator"] == ["Jane Doe"]
+    assert (
+        assertions["stds.iptc.photo-metadata"]["data"]["dc:rights"]
+        == "copyright holder"
+    )
+
+
+def test_generates_update_claim():
+    claim = _claim.generate_update()
+    assert claim["vendor"] == "starlinglab"
+
+
+def test_generates_store_claim():
+    claim = _claim.generate_store("a-made-up-cid")
+    assertions = _claim.assertions_by_label(claim)
+    assert (
+        assertions["org.starlinglab.storage.ipfs"]["data"]["starling:IpfsCid"]
+        == "a-made-up-cid"
+    )
