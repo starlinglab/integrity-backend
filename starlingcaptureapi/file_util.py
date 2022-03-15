2137252c50e1b0b3655b88c6d270458800888e64
makeworld <25111343+makeworld-the-better-one@users.noreply.github.com>
2022-03-15 10:44:27 -0400

    File encryption and decryption with AES (#66)

--- starlingcaptureapi/file_util.py
+++ starlingcaptureapi/file_util.py
@@ -1,4 +1,7 @@
+from .crypto_util import AESCipher
 from . import config
+
+from Crypto.Cipher import AES
 from hashlib import sha256
 
 import errno
@@ -58,6 +61,74 @@ class FileUtil:
                 hasher.update(byte_block)
             return hasher.hexdigest()
         # TODO: handle error (image not found, etc.)
+
+    def encrypt(self, key, file_path, enc_file_path):
+        """Writes an encrypted version of the file to disk.
+
+        Args:
+            key: an AES-256 key as bytes (32 bytes)
+            file_path: the path to the unencrypted file
+            enc_file_path: the path where the encrypted file will go
+
+        Raises:
+            Any AES errors
+            Any errors during file creation or I/O
+        """
+
+        cipher = AESCipher(key)
+
+        # Read and encrypt 32 KiB at a time
+        buffer_size = 32 * 1024
+
+        with open(file_path, "rb") as dec, open(enc_file_path, "wb") as enc:
+            # Begin file with the Initialization Vector.
+            # This is a standard way of storing the IV in a file for AES-CBC,
+            # and it's what the lit-js-sdk does.
+            enc.write(cipher.iv)
+
+            while True:
+                data = dec.read(buffer_size)
+
+                if len(data) % AES.block_size != 0 or len(data) == 0:
+                    # This is the final block in the file
+                    # It's not a multiple of the AES block size so it must be padded
+                    enc.write(cipher.encrypt_last_block(data))
+                    break
+
+                enc.write(cipher.encrypt(data))
+
+    def decrypt(self, key, file_path, dec_file_path):
+        """Writes a decrypted version of the file to disk.
+
+        Args:
+            key: an AES-256 key as bytes (32 bytes)
+            file_path: the path to the encrypted file
+            dec_file_path: the path where the decrypted file will go
+
+        Raises:
+            Any AES errors
+            Any errors during file creation or I/O
+        """
+
+        # Read and decrypt 16 KiB at a time
+        buffer_size = 16 * 1024
+
+        with open(file_path, "rb") as enc, open(dec_file_path, "wb") as dec:
+            # Get IV
+            iv = enc.read(16)
+            cipher = AESCipher(key, iv)
+
+            data = enc.read(buffer_size)
+            while True:
+                prev_data = data
+                data = enc.read(buffer_size)
+
+                if len(data) == 0:
+                    # prev_data is the final block in the file and is therefore padded
+                    dec.write(cipher.decrypt_last_block(prev_data))
+                    break
+
+                dec.write(cipher.decrypt(prev_data))
 
     @staticmethod
     def digest_cidv1(self, file_path):
