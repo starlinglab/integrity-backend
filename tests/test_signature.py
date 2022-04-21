5f5be8965c1498ffca7d5c526315f8854005ec39
makeworld <makeworld@protonmail.com>
2022-04-21 11:10:28 -0400

    Verify Zion session sigs

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -8,3 +8,11 @@ def test_verify_androidopenssl():
     with open("tests/assets/starling-capture-test/meta.json", "r") as f:
         meta_raw = f.read()
     assert signature._verify_androidopenssl(meta_raw, signatures[0]) == True
+
+
+def test_verify_zion():
+    with open("tests/assets/starling-capture-test/signature.json", "r") as f:
+        signatures = json.load(f)
+    with open("tests/assets/starling-capture-test/meta.json", "r") as f:
+        meta_raw = f.read()
+    assert signature._verify_zion(meta_raw, signatures[1]) == True
