edfad0a865416c21d5f7287fedc288aa13a26892
makeworld <makeworld@protonmail.com>
2022-05-26 11:40:14 -0400

    Update tests to use working sigs from 1.8.0 app

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -10,12 +10,28 @@ def test_verify_androidopenssl_session():
     assert signature._verify_androidopenssl(meta_raw, signatures[0]) == True
 
 
+def test_verify_bad_androidopenssl_session():
+    with open("tests/assets/starling-capture/bad/session/signature.json", "r") as f:
+        signatures = json.load(f)
+    with open("tests/assets/starling-capture/bad/session/meta.json", "r") as f:
+        meta_raw = f.read()
+    assert signature._verify_androidopenssl(meta_raw, signatures[0]) == False
+
+
 def test_verify_androidopenssl_no_session():
     with open("tests/assets/starling-capture/good/no-session/signature.json", "r") as f:
         signatures = json.load(f)
     with open("tests/assets/starling-capture/good/no-session/meta.json", "r") as f:
         meta_raw = f.read()
     assert signature._verify_androidopenssl(meta_raw, signatures[0]) == True
+
+
+def test_verify_bad_androidopenssl_no_session():
+    with open("tests/assets/starling-capture/bad/no-session/signature.json", "r") as f:
+        signatures = json.load(f)
+    with open("tests/assets/starling-capture/bad/no-session/meta.json", "r") as f:
+        meta_raw = f.read()
+    assert signature._verify_androidopenssl(meta_raw, signatures[0]) == False
 
 
 def test_verify_zion_session():
@@ -26,9 +42,25 @@ def test_verify_zion_session():
     assert signature._verify_zion(meta_raw, signatures[1]) == True
 
 
+def test_verify_bad_zion_session():
+    with open("tests/assets/starling-capture/bad/session/signature.json", "r") as f:
+        signatures = json.load(f)
+    with open("tests/assets/starling-capture/bad/session/meta.json", "r") as f:
+        meta_raw = f.read()
+    assert signature._verify_zion(meta_raw, signatures[1]) == False
+
+
 def test_verify_zion_no_session():
     with open("tests/assets/starling-capture/good/no-session/signature.json", "r") as f:
         signatures = json.load(f)
     with open("tests/assets/starling-capture/good/no-session/meta.json", "r") as f:
         meta_raw = f.read()
     assert signature._verify_zion(meta_raw, signatures[1]) == True
+
+
+def test_verify_bad_zion_no_session():
+    with open("tests/assets/starling-capture/bad/no-session/signature.json", "r") as f:
+        signatures = json.load(f)
+    with open("tests/assets/starling-capture/bad/no-session/meta.json", "r") as f:
+        meta_raw = f.read()
+    assert signature._verify_zion(meta_raw, signatures[1]) == False
